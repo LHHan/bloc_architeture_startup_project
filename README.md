0668b84a4b5c4f6a5b8c1ff6b62c3d5754c647b4
han-lh <hoanghan.le.95@gmail.com>
2021-06-08 11:15:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bloc_architeture_startup_project
+ BLoC Architeture Startup Project
---
+++ android/app/src/main/kotlin/com/example/bloc_architeture_startup_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_architeture_startup_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
